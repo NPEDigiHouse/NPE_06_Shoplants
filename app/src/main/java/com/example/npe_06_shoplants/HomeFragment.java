7942c4cd4615e1f28a710624efd6415ac6f4e4a9
yukiao <lembang19sony@gmail.com>
2021-05-29 13:02:02 +0800

    Add static database

--- app/src/main/java/com/example/npe_06_shoplants/HomeFragment.java
+++ app/src/main/java/com/example/npe_06_shoplants/HomeFragment.java
@@ -2,13 +2,21 @@ package com.example.npe_06_shoplants;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.npe_06_shoplants.models.Plant;
+
+import java.util.List;
+
 public class HomeFragment extends Fragment {
+
+    private RecyclerView recyclerView;
+    private List<Plant> list;
 
     public static HomeFragment newInstance() {
         HomeFragment homeFragment = new HomeFragment();
@@ -26,6 +34,6 @@ public class HomeFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+       return inflater.inflate(R.layout.fragment_home, container, false);
     }
 }
---
+++ app/src/main/java/com/example/npe_06_shoplants/data/PlantsData.java
@@ -0,0 +1,44 @@
+package com.example.npe_06_shoplants.data;
+
+import com.example.npe_06_shoplants.models.Plant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlantsData {
+    private static String plantName [] = {
+            "Bonsai",
+            "Bambu",
+            "Lavender",
+            "Red Plant",
+            "Kaktus"
+    };
+
+    private static String imageURL [] = {
+        "https://firebasestorage.googleapis.com/v0/b/shoplants-c2e1e.appspot.com/o/plants%2Fkisspng-flowerpot-bonsai-loudspeaker-plant-potted-plants-5a997bbc396190.8508827815200081242351.png?alt=media&token=7c783637-4ad8-46b7-b110-bca770f87ed8",
+            "https://firebasestorage.googleapis.com/v0/b/shoplants-c2e1e.appspot.com/o/plants%2Fkisspng-flowerpot-houseplant-bamboo-potted-plants-decorate-pictures-5a97eb48359c68.2720286015199056082196.png?alt=media&token=645f65d3-d430-4df9-8fa3-f58709c4d6ac",
+            "https://firebasestorage.googleapis.com/v0/b/shoplants-c2e1e.appspot.com/o/plants%2Flavender%202.png?alt=media&token=fab63555-15d8-4536-b8fc-193816b74a12",
+            "https://firebasestorage.googleapis.com/v0/b/shoplants-c2e1e.appspot.com/o/plants%2Fkisspng-houseplant-flowerpot-indoor-plant-potted-plants-5a8b8a300dda37.9780680715190943200568.png?alt=media&token=b52a026a-fc6f-4e4c-98b9-17a4e4156f8e",
+            "https://firebasestorage.googleapis.com/v0/b/shoplants-c2e1e.appspot.com/o/plants%2Fkaktus.png?alt=media&token=63908ec1-7991-4b30-8836-979d72b42274"
+    };
+
+    private static int price [] = {
+            50000,
+            60000,
+            70000,
+            80000,
+            90000
+    };
+
+    public static List<Plant> getPlantsData(){
+        List<Plant> list = new ArrayList<>();
+        for(int i=0; i<plantName.length; i++){
+            Plant plant = new Plant();
+            plant.setName(plantName[i]);
+            plant.setImageUrl(imageURL[i]);
+            plant.setPrice(price[i]);
+        }
+
+        return list;
+    }
+}

4759ce7055dc79a129541cdedc8459b33870a0d3
yukiao <lembang19sony@gmail.com>
2021-05-29 13:16:41 +0800

    Add sign in with email and password feature

--- app/src/main/java/com/example/npe_06_shoplants/SignInActivity.java
+++ app/src/main/java/com/example/npe_06_shoplants/SignInActivity.java
@@ -1,19 +1,28 @@
 package com.example.npe_06_shoplants;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.button.MaterialButton;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import org.jetbrains.annotations.NotNull;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -21,6 +30,8 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     private ImageView googleSignIn;
     private GoogleSignInClient mGoogleSignInClient;
     private Button login;
+
+    private EditText etEmail, etPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,24 +47,59 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
         ImageView ivSignInWithEmail = findViewById(R.id.ivSignInWithEmail);
         ivSignInWithEmail.setOnClickListener(this);
 
-        login = findViewById(R.id.btnSignIn);
-        googleSignIn = findViewById(R.id.ivSignInWithEmail);
+        etEmail = findViewById(R.id.etUsernameOrEmail);
+        etPassword = findViewById(R.id.etPassword);
+
         mAuth = FirebaseAuth.getInstance();
+    }
 
-
-
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseUser user = mAuth.getCurrentUser();
+        if(user != null){
+            startActivity(new Intent(SignInActivity.this, MainActivity.class));
+            finish();
+        }
     }
 
 
     @Override
     public void onClick(View view) {
-        if (view.getId() == R.id.btnSignIn) {
-            Intent mainIntent = new Intent(SignInActivity.this, MainActivity.class);
-            startActivity(mainIntent);
-        } else if (view.getId() == R.id.tvSignUp) {
-            Intent signUpIntent = new Intent(SignInActivity.this, SignUpActivity.class);
-            startActivity(signUpIntent);
+        switch(view.getId()){
+            case R.id.btnSignIn:
+                manualSignIn();
+                break;
+            case R.id.tvSignUp:
+                startActivity(new Intent(SignInActivity.this, SignUpActivity.class));
         }
+    }
+
+    private void manualSignIn() {
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if(email.isEmpty()){
+            etEmail.setError("Please insert password");
+            etEmail.requestFocus();
+            return;
+        }
+        if(password.isEmpty()){
+            etPassword.setError("Please insert password");
+            etPassword.requestFocus();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull @NotNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    startActivity(new Intent(SignInActivity.this, MainActivity.class));
+                    finish();
+                }else{
+                    Toast.makeText(SignInActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
 }
